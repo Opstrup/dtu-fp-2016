8a357e5f62ce65cf80c18ce79417bf473855a7c3
Anders Opstrup <aopstrup@gmail.com>
2016-11-22 16:18:51 +0100

    started on mandatory assignment 04

---
+++ mandatory_assignment04/mandatory_assignment04.fs
@@ -0,0 +1,12 @@
+// ---------------------------------------------------------------
+// Mandatory assignment 4
+// Course: Functional programming - 02157
+// ---------------------------------------------------------------
+// Student info:
+// Name: Anders Holmgaard Opstrup
+// Student no. s160148
+// ---------------------------------------------------------------
+// Collaborator info:
+// Name: Thomas Juel Neergaard
+// Student no. s155991
+// ---------------------------------------------------------------
