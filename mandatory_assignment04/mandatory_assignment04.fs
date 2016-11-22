67883eb229406b20898f6ddc90de09f43c1c83db
Anders Opstrup <aopstrup@gmail.com>
2016-11-22 17:22:24 +0100

    finished part 1 and 2

--- mandatory_assignment04/mandatory_assignment04.fs
+++ mandatory_assignment04/mandatory_assignment04.fs
@@ -10,3 +10,42 @@
 // Name: Thomas Juel Neergaard
 // Student no. s155991
 // ---------------------------------------------------------------
+
+// Part 1.
+type Prop = 
+    | P of string
+    | Dis of Prop * Prop
+    | Con of Prop * Prop
+    | Neg of Prop
+
+// Part 2.
+let propositionConverter = function
+    | Dis (x, y) -> Dis (x, y)
+    | Con (x, y) -> Con (x, y)
+    | Neg x ->
+        match x with
+        | Dis (x, y) -> Con(Neg x, Neg y) 
+        | Con (x, y) -> Dis(Neg x, Neg y)
+        | Neg x -> x
+        | _ -> x
+    | P _ as x -> x
+
+// Part 3.
+
+// ----------------------- TESTS ----------------------- // 
+let atomA = P "a"
+let atomB = P "b"
+let negA = Neg atomA
+let AandB = Con(atomA, atomB)
+let AorB = Dis(atomA, atomB)
+let notAandB = Neg AandB
+let notAorB = Neg AorB
+
+propositionConverter negA
+    |> printf "The result is %A \n"
+
+propositionConverter notAandB
+    |> printf "The result is %A \n"
+
+propositionConverter notAorB
+    |> printf "The result is %A \n"
